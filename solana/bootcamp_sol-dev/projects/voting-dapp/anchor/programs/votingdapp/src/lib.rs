3ea58474439dc177165a0b8b9fa83401aa553d45
Panther <prathamjaiswal204@gmail.com>
2025-08-28 19:52:03 +0530

    additions

--- solana/bootcamp_sol-dev/projects/voting-dapp/anchor/programs/votingdapp/src/lib.rs
+++ solana/bootcamp_sol-dev/projects/voting-dapp/anchor/programs/votingdapp/src/lib.rs
@@ -8,55 +8,141 @@ declare_id!("coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhpewyzRVF");
 pub mod votingdapp {
     use super::*;
 
-    pub fn initialize_poll(ctx: Context<InitializePoll>,
-                      poll_id: u64,
-                      poll_name: String,
-                      poll_description: String,
-                      poll_start_time: u64,
-                      poll_end_time: u64,
-                      total_votes: u64,
-                    ) -> Result<()> {
-      let poll = &mut ctx.accounts.poll;
-      poll.poll_id = poll_id;
-      poll.poll_name = poll_name;
-      poll.poll_description = poll_description;
-      poll.total_votes = total_votes;
-      poll.poll_start_time = poll_start_time;
-      poll.poll_end_time = poll_end_time;
+    pub fn initialize_poll(
+        ctx: Context<InitializePoll>,
+        poll_id: u64,
+        poll_name: String,
+        poll_description: String,
+        poll_start_time: u64,
+        poll_end_time: u64,
+        total_candidates: u64,
+    ) -> Result<()> {
+        let poll = &mut ctx.accounts.poll;
+        poll.poll_id = poll_id;
+        poll.poll_name = poll_name;
+        poll.poll_description = poll_description;
+        poll.total_candidates = total_candidates;
+        poll.poll_start_time = poll_start_time;
+        poll.poll_end_time = poll_end_time;
+        Ok(())
+    }
+
+    pub fn initialize_candidate(
+        ctx: Context<InitializeCandidate>,
+        _candidate_name: String,
+        _poll_id: u64,
+    ) -> Result<()> {
+        // Initialize candidate logic here
+        let candidate = &mut ctx.accounts.candidate;
+        let poll = &mut ctx.accounts.poll;
+        poll.total_candidates += 1;
+        candidate.candidate_name = _candidate_name;
+        candidate.candidate_votes = 0;
+        Ok(())
+    }
+
+    pub fn vote(ctx: Context<Vote>, _candidate_name: String, _poll_id: u64) -> Result<()> {
+        // Voting logic here
+        let candidate = &mut ctx.accounts.candidate;
+        let current_time = Clock::get()?.unix_timestamp;
+
+        if current_time > (ctx.accounts.poll.poll_end_time as i64) {
+            return Err(ErrorCode::VotingEnded.into());
+        }
+
+        if current_time <= (ctx.accounts.poll.poll_start_time as i64) {
+            return Err(ErrorCode::VotingNotStarted.into());
+        }
+
+        candidate.candidate_votes += 1;
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
+#[instruction(candidate_name: String, poll_id: u64)]
+pub struct Vote<'info> {
+    pub signer: Signer<'info>,
+
+    #[account(
+      seeds = [poll_id.to_le_bytes().as_ref()],
+      bump,
+    )]
+    pub poll: Account<'info, Poll>,
+
+    #[account(
+    seeds = [candidate_name.as_bytes(), poll_id.to_le_bytes().as_ref()],
+    bump,
+    )]
+    pub candidate: Account<'info, Candidate>,
+}
+
+#[derive(Accounts)]
+#[instruction(candidate_name: String, poll_id: u64)]
+pub struct InitializeCandidate<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    #[account(
+      seeds = [poll_id.to_le_bytes().as_ref()],
+      bump,
+    )]
+    pub poll: Account<'info, Poll>,
+
+    #[account(init,
+    payer = signer,
+    space = 8 + Candidate::INIT_SPACE,
+    seeds = [candidate_name.as_bytes(), poll_id.to_le_bytes().as_ref()],
+    bump,
+    )]
+    pub candidate: Account<'info, Candidate>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct Candidate {
+    #[max_len(32)]
+    pub candidate_name: String,
+    pub candidate_votes: u64,
+}
+
+#[derive(Accounts)]
 #[instruction(poll_id: u64)]
 pub struct InitializePoll<'info> {
-  #[account(mut)]
-  pub signer: Signer<'info>,
-  #[account(init,
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    #[account(init,
     payer = signer,
     space = 8 + Poll::INIT_SPACE,
     seeds = [poll_id.to_le_bytes().as_ref()],
     bump,
   )]
-  pub poll: Account<'info, Poll>,
+    pub poll: Account<'info, Poll>,
 
-  pub system_program: Program<'info, System>,
+    pub system_program: Program<'info, System>,
 }
 
 #[account]
 #[derive(InitSpace)]
 pub struct Poll {
-  pub poll_id: u64,
-  #[max_len(32)]
-  pub poll_name: String,
-  #[max_len(255)]
-  pub poll_description: String,
-  pub poll_start_time: u64,
-  pub poll_end_time: u64,
-  pub total_votes: u64,
+    pub poll_id: u64,
+    #[max_len(32)]
+    pub poll_name: String,
+    #[max_len(255)]
+    pub poll_description: String,
+    pub poll_start_time: u64,
+    pub poll_end_time: u64,
+    pub total_candidates: u64,
 }
 
-#[account]
-pub struct Vote {
-  
+#[error_code]
+pub enum ErrorCode {
+    #[msg("Voting has not started yet")]
+    VotingNotStarted,
+    #[msg("Voting has ended")]
+    VotingEnded,
 }
--- solana/bootcamp_sol-dev/projects/voting-dapp/anchor/tests/votingdapp.spec.ts
+++ solana/bootcamp_sol-dev/projects/voting-dapp/anchor/tests/votingdapp.spec.ts
@@ -1,22 +1,129 @@
-import * as anchor from '@coral-xyz/anchor'
-import { Program } from '@coral-xyz/anchor'
-import { Keypair, PublicKey } from '@solana/web3.js'
-import { Votingdapp } from '../target/types/votingdapp'
-import { startAnchor } from 'solana-bankrun'
-import { BankrunProvider } from 'anchor-bankrun'
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
+import { Keypair, PublicKey } from "@solana/web3.js";
+import { Votingdapp } from "../target/types/votingdapp";
+import { startAnchor } from "solana-bankrun";
+import { BankrunProvider } from "anchor-bankrun";
+import { VotingdappCreate } from "../../src/components/votingdapp/votingdapp-ui";
+import { getVotingdappProgram } from "@project/anchor";
 
-const IDL = require('../target/idl/votingdapp.json')
+const IDL = require("../target/idl/votingdapp.json");
 
 const votingAddress = new PublicKey(
-  'coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhpewyzRVF'
-)
+  "coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhpewyzRVF"
+);
 
-describe('votingdapp', () => {
-  it('Initialize poll', async () => {
-    const context = await startAnchor(
-      'test/anchor-example',
-      [{ name: 'votingdapp', programId: votingAddress }],
+describe("Voting", () => {
+  let context;
+  let provider;
+  let votingProgram: Program<Votingdapp>;
+
+  beforeAll(async () => {
+    context = await startAnchor(
+      "",
+      [{ name: "votingdapp", programId: votingAddress }],
       []
-    )
-  })
-})
+    );
+    provider = new BankrunProvider(context);
+
+    votingProgram = new Program<Votingdapp>(IDL, provider);
+  });
+
+  it("Initialize poll", async () => {
+    context = await startAnchor(
+      "",
+      [{ name: "votingdapp", programId: votingAddress }],
+      []
+    );
+    provider = new BankrunProvider(context);
+
+    votingProgram = new Program<Votingdapp>(IDL, provider);
+
+    await votingProgram.methods
+      .initializePoll(
+        new anchor.BN(1),
+        "What is your favorite type of peanut butter?",
+        "Please select your favorite type of peanut butter from the options below.",
+        new anchor.BN(0),
+        new anchor.BN(1821246480),
+        new anchor.BN(100) // Added missing sixth argument
+      )
+      .rpc();
+
+    const [pollAddress] = PublicKey.findProgramAddressSync(
+      [new anchor.BN(1).toArrayLike(Buffer, "le", 8)],
+      votingAddress
+    );
+
+    const poll = await votingProgram.account.poll.fetch(pollAddress);
+
+    console.log("Poll initialized:", poll);
+
+    expect(poll.pollId.toNumber()).toBe(1);
+    expect(poll.pollName).toBe("What is your favorite type of peanut butter?");
+    expect(poll.pollDescription).toBe(
+      "Please select your favorite type of peanut butter from the options below."
+    );
+    expect(poll.pollStartTime.toNumber()).toBeLessThan(
+      poll.pollEndTime.toNumber()
+    );
+    expect(poll.pollEndTime.toNumber()).toBe(1821246480);
+  });
+
+  it("Initialize candidates", async () => {
+    const [pollAddress] = PublicKey.findProgramAddressSync(
+      [new anchor.BN(1).toArrayLike(Buffer, "le", 8)],
+      votingAddress
+    );
+
+    await votingProgram.methods
+      .initializeCandidate("Smooth", new anchor.BN(1))
+      .rpc();
+
+    await votingProgram.methods
+      .initializeCandidate("Crunchy", new anchor.BN(1))
+      .rpc();
+
+    const [smoothAddress, bump] = PublicKey.findProgramAddressSync(
+      [
+        Buffer.from("candidate"),
+        Buffer.from("Smooth"),
+        new anchor.BN(1).toArrayLike(Buffer, "le", 8),
+      ],
+      votingAddress
+    );
+    const smoothCandidate = await votingProgram.account.candidate.fetch(
+      smoothAddress
+    );
+    console.log("Smooth candidate initialized:", smoothCandidate);
+
+    const [crunchyAddress, bump2] = PublicKey.findProgramAddressSync(
+      [
+        Buffer.from("candidate"),
+        Buffer.from("Crunchy"),
+        new anchor.BN(1).toArrayLike(Buffer, "le", 8),
+      ],
+      votingAddress
+    );
+    console.log(
+      "crunchyAddress",
+      crunchyAddress.toBase58(),
+      "bump",
+      bump2.toString()
+    );
+
+    // const acct = await provider.connection.getAccountInfo(crunchyAddress);
+    // console.log("getAccountInfo crunchy:", !!acct, acct ? acct.lamports : null);
+    const crunchyCandidate = await votingProgram.account.candidate.fetch(
+      crunchyAddress
+    );
+    console.log("Crunchy candidate initialized:", crunchyCandidate);
+
+    expect(crunchyCandidate.candidateVotes.toNumber()).toBe(0);
+    expect(smoothCandidate.candidateVotes.toNumber()).toBe(0);
+  });
+
+  it("vote", async () => {
+    await votingProgram.methods.vote("Smooth", new anchor.BN(1)).rpc();
+  });
+});
